e2e33201848647fd984dfea293a2534af05cfbfd
Aurelio Bernal <aurelio@bernalr.com>
2026-04-24 11:46:08 -0600

    add virtual source, ordering, and permission flags to PerformanceProfile

--- app/src/main/java/com/aure/androidtuner/model/PerformanceProfile.kt
+++ app/src/main/java/com/aure/androidtuner/model/PerformanceProfile.kt
@@ -3,6 +3,7 @@ package com.aure.androidtuner.model
 enum class ProfileSource {
     BUNDLED,
     USER,
+    VIRTUAL,
 }
 
 data class PerformanceProfile(
@@ -11,4 +12,7 @@ data class PerformanceProfile(
     val maxFrequencies: Map<Int, Int>,
     val source: ProfileSource,
     val isResetProfile: Boolean = false,
+    val order: Int = 0,
+    val isEditable: Boolean = source == ProfileSource.USER,
+    val isDeletable: Boolean = source == ProfileSource.USER,
 )
