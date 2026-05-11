4f98165bd8277f5f2a852de3cb16cb702a51138c
Zulux91 <leodiaz91.ld@gmail.com>
2026-05-10 21:25:19 -0500

    chore: bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.aure.clustertune"
         minSdk = 31
         targetSdk = 34
-        versionCode = 300
-        versionName = "0.3.0"
+        versionCode = 301
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
